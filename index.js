739a9f1d58d7439352904bf1184b18ba565d1913
luis11 <luis_enomine1@outlook.es>
2018-07-12 13:23:27 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 
 app.get('/',function(req,res){
-	res.send('Hello world');
+	res.send('Hola mundo');
 	});
 	app.listen(8080,function(){
 		console.log('app listening on port 8080');
